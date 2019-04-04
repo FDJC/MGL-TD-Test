f36831d88674f670a4fda84c9a15fce8d3a07af0
Fontaine Dylan <dfontaine@TC405-112-04.insa-lyon.fr>
2019-04-04 10:27:08 +0200

    version 2

--- roman-numbers/src/main/java/romannumberconverter/RomanNumberConverter.java
+++ roman-numbers/src/main/java/romannumberconverter/RomanNumberConverter.java
@@ -1,67 +1,77 @@
 package romannumberconverter;
-
+import java.util.*;
 public class RomanNumberConverter {
 
   public int convert(String romanNumber) {
-    int number=0;
+     Dictionary<String, Integer> dic= new Hashtable<String, Integer>();
+     dic.put("I",1);
+     dic.put("V",5);
+     dic.put("X",10);
+     dic.put("L",50);
+     dic.put("C",100);
+     dic.put("D",500);
+     dic.put("M",1000);
 
-    for ( char c : romanNumber.toCharArray() ) {
+     int number=0;
+     char[] mot = romanNumber.toCharArray();
+     if(romanNumber.contains("IIII")||romanNumber.contains("VVVV")||romanNumber.contains("XXXX")||romanNumber.contains("LLLL")||romanNumber.contains("CCCC")||romanNumber.contains("DDDD")){
+       throw new IllegalArgumentException("argument incorrect");
+     }
+
+     for (int i = 0; i < mot.length; i++) {
+       if((i+1)!=mot.length){
+         if (dic.get(String.valueOf(mot[i])) < dic.get(String.valueOf(mot[i+1]))){ //si on a une lettre plus faible avant une autre
+           number-=dic.get(String.valueOf(mot[i]));
+         }
+         else{ //si la lettre est placé "normalement", on ajoute sa valeur
+           number+=dic.get(String.valueOf(mot[i]));
+         }
+       }
+       else{
+         number+=dic.get(String.valueOf(mot[i]));
+       }
+
+     }
+
+     if(number<=0){
+       throw new IllegalArgumentException("argument incorrect");
+
+     }
+
+     return number;
+   }
+
+   public String convert(int number){
+     String mot="";
+
+     if(number<=0){
+       throw new IllegalArgumentException("argument incorrect");
+     }
+     if(number==1){
+       mot="I";
+     }
+     if(number == 5){
+       mot="V";
+     }
+     if(number==10){
+       mot="X";
+     }
+     if(number == 50){
+       mot="L";
+     }
+     if(number == 100){
+       mot="C";
+     }
+     if(number == 500){
+       mot="D";
+     }
+     if(number == 1000){
+       mot="M";
+     }
 
 
-      if(c == 'I'){
-        number+=1;
-      }
-      if(c == 'V'){
-        number+=5;
-      }
-      if(c == 'X'){
-        number+=10;
-      }
-      if(c == 'L'){
-        number+=50;
-      }
-      if(c == 'C'){
-        number+=100;
-      }
-      if(c == 'D'){
-        number+=500;
-      }
-      if(c == 'M'){
-        number+=1000;
-      }
-
-    }
-
-    return number;
-  }
-
-  public String convert(int number){
-    String mot="";
-    if(number==1){
-      mot="I";
-    }
-    if(number == 5){
-      mot="V";
-    }
-    if(number==10){
-      mot="X";
-    }
-    if(number == 50){
-      mot="L";
-    }
-    if(number == 100){
-      mot="C";
-    }
-    if(number == 500){
-      mot="D";
-    }
-    if(number == 1000){
-      mot="M";
-    }
-
-
-    return mot;
-  }
+     return mot;
+   }
 
 
 
